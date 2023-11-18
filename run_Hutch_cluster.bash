c22772df72ce92d4da6c497890e8cb6aac2c3166
Brendan Larsen <blarsen@rhino01.fhcrc.org>
2023-11-18 12:28:40 -0800

    cleaned config files and fixed formatting, improved parameter values, and readded back functional shifts between selections in different cell types

--- run_Hutch_cluster.bash
+++ run_Hutch_cluster.bash
@@ -2,4 +2,4 @@
 #
 #SBATCH -c 16
 
-snakemake -c 32 --use-conda --rerun-incomplete -s dms-vep-pipeline-3/Snakefile
+snakemake -c 16 --use-conda --latency-wait 60 --rerun-incomplete -s dms-vep-pipeline-3/Snakefile
